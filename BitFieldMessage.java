1d396d5c53368f0924af35a904c86f612ce22672
Andrea Sanchez <andreasanchez@Andreas-MacBook-Air-4.local>
2024-03-24 19:52:53 -0400

    Added byte converters for piece and bitfield messages to read index

--- BitFieldMessage.java
+++ BitFieldMessage.java
@@ -1,3 +1,27 @@
-//the payload for the bitfield has to be broken up
+//Each bit in the bitfield payload represents whether the peer has the corresponding piece or not. The
+//first byte of the bitfield corresponds to piece indices 0 – 7 from high bit to low bit,
+//respectively. The next one corresponds to piece indices 8 – 15, etc. Spare bits at the end
+//are set to zero. Peers that don’t have anything yet may skip a ‘bitfield’ message.
+
 public class BitFieldMessage {
+    // i want to convert the byte to integer to see the piece that the bitfield is representing
+    public static int byteArrayToInt(byte[] bytes){
+        int value = 0;
+        for(int i = 0; i < 4; i++)
+        {
+            if(i == 0){
+                value += (bytes[i] & 0xFF) << 24;
+            }
+            else if(i == 1){
+                value += (bytes[i] & 0xFF) << 16;
+            }
+            else if(i == 2){
+                value += (bytes[i] & 0xFF) << 8;
+            }
+            else{
+                value += (bytes[i] & 0xFF);
+            }
+        }
+        return value;
+    }
 }
--- ByteManager.java
+++ ByteManager.java
@@ -1,3 +1,8 @@
 public class ByteManager {
-    //this is used to convert bytes into
+    //this is used to convert byte arrays to bit fields
+    //this may be needed with bitfields, piece messages and handshake messages
+    //this is used to compare the indexes of the pieces that the other peers have
+
+
+
 }
