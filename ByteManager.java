077fa39f92e783041efea627f0927c9b3c037a1f
Pedro Camargo <pedrocamargo@Pedros-MacBook-Pro.local>
2024-04-18 16:41:05 -0400

    Cleaning up clutter from midpoint submission

--- ByteManager.java
+++
@@ -1,8 +0,0 @@
-public class ByteManager {
-    //this is used to convert byte arrays to bit fields
-    //this may be needed with bitfields, piece messages and handshake messages
-    //this is used to compare the indexes of the pieces that the other peers have
-
-
-
-}
